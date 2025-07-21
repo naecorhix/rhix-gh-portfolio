43af6fdfca4d5ce2ae0bade2b46dfafe4b337e28
naecorhix <johnhenrixgillo@gmail.com>
2025-07-21 19:35:55 +0800

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div>
+        <div className="badge badge-soft badge-primary badge-xl	">Primary</div>
+      </div>
+    </>
+  )
+}
+
+export default App
